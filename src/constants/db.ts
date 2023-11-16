b8af3060315ecac9fbe419e7e313985d52e6687d
Odonno <bottiau.david@laposte.net>
2023-11-16 12:09:19 +0100

    🔧 add env variables for surrealdb connection

--- src/constants/db.ts
+++ src/constants/db.ts
@@ -1,4 +1,4 @@
-export const SURREAL_ENDPOINT = "http://localhost:8000";
-export const NS = "test";
-export const DB = "test";
+export const SURREAL_ENDPOINT = import.meta.env.VITE_SURREAL_ENDPOINT;
+export const NS = import.meta.env.VITE_SURREAL_NS;
+export const DB = import.meta.env.VITE_SURREAL_DB;
 export const USER_SCOPE = "user_scope";
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SURREAL_ENDPOINT: string;
+  readonly VITE_SURREAL_NS: string;
+  readonly VITE_SURREAL_DB: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
